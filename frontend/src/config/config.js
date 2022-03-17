0e41436fb94592eabdc78684ecf0923bfb4f3acf
rushabh-mehta <rushabh123@gmail.com>
2022-03-16 21:40:08 -0700

    config changes

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,6 +1,5 @@
 const config = {
-        baseUrl:'http://localhost:3001'
-        // baseUrl:'http://18.191.53.212:3001'
+        baseUrl:'http://18.191.53.212:3001'
     };
 
 export default config;
