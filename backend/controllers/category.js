e56181c9ca018b3dd72b43c21cae0707528da4cd
rushabh-mehta <rushabh123@gmail.com>
2022-03-23 20:42:43 -0700

    Bug fix - category still using mysql db

--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const config =  require('config');
-const { Category } = require("../models/category");
+const { Category } = require("../services/category");
 const encrypt = require("../services/encrypt");
 const router = express.Router();
 const passport = require('passport');
