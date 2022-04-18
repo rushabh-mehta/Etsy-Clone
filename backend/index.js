73cd53ad2ec5a73241a25c565a1e7bfd18b3cd01
rushabh-mehta <rushabh123@gmail.com>
2022-04-18 03:10:09 -0700

    removed unwanted home component

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,6 @@ const mysql = require("mysql2");
 const auth = require("./middleware/auth");
 const register = require("./controllers/register");
 const login = require("./controllers/login");
-const home = require("./controllers/home");
 const user = require("./controllers/user");
 const country = require("./controllers/country");
 const shop = require("./controllers/shop");
@@ -29,7 +28,6 @@ app.use(passport.initialize());
 
 app.use('/api/register',register);
 app.use('/api/login',login);
-app.use('/api/home',home);
 app.use('/api/user',user);
 app.use('/api/country',country);
 app.use('/api/shop',shop);
